945d555148699872aa6ec073b77ce3be60be8888
Vladislav <vladbezv06@gmail.com>
2025-02-19 21:14:13 +0300

    2 лабораторная

---
+++ lab2/1/1.cpp
@@ -0,0 +1,39 @@
+﻿#include <iostream>
+#include <ctime>
+#include <cstdlib>
+
+using namespace std;
+
+int main(){
+    setlocale(0, "rus");
+    srand(time(0));
+    int mas[10];
+    int* p = &mas[0];
+    for (int i = 0; i < 10; i++) {
+        *(p + i) = 1 + rand() % 10;
+    }
+    cout << "Массив:\n";
+    for (int i = 0; i < 10; i++) {
+        cout << *(p + i) << " ";
+    }
+    cout << "\n";
+    int locm;
+    int n;
+    if (*p > *(p + 1)) {
+        locm = *p;
+        n = 1;
+    }
+    for (int i = 1; i < 9; i++) {
+        if (*(p + i) > *(p + i + 1) && *(p + i) > *(p + i - 1)) {
+            locm = *(p + i);
+            n = i + 1;
+        }
+    }
+    if (*(p+9) > *(p + 8)) {
+        locm = *(p + 9);
+        n = 10;
+    }
+    
+    cout << "Локальный максимум: " << locm << " номер: " << n << "\n";
+    system("PAUSE");
+}
---
+++ lab2/2/2.cpp
@@ -0,0 +1,36 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    setlocale(0, "rus");
+    float mas[10];
+    float* p = &mas[0];
+    for (int i = 0; i < 10; i++) {
+        while (true) {
+            cout << "Введите число " << i + 1 << " можно вещественное: \n";
+            cin >> *(p + i);
+            if (cin.fail()) {
+                cin.clear();
+                cin.ignore(100000000, '\n');
+                cout << "Это не число. Введите снова \n";
+            }
+            else {
+                break;
+            }
+        }
+    }
+    cout << "Массив: ";
+    for (int i = 0; i < 10; i++) {
+        cout << *(p + i) << " ";
+    }
+    cout << "\n";
+    int n = 0;
+    for (int i = 0; i < 10; i++) {
+        if (*(p + i) < 0) {
+            n = n + 1;
+        }
+    }
+    cout << "Число отрицательных элементов " << n << "\n";
+    system("PAUSE");
+}
